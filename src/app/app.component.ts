39334f373de71728222e3e852a6055de093bad60
Calehb69 <isarhu@gmail.com>
2021-12-10 15:26:46 +0100

    Exo6Liste-Terminé
    Partie 6 de l'exercice 6 complètement terminée

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { Statistique } from './models/statistique';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-statRugby = new Statistique(
-    'rugbysfezad-10-efqfe',
-    'Rugby Statistique',
-    '42G'
+public tabStat: Statistique[] = [    
+    new Statistique('rugbysfezad-10-efqfe', 'Rugby Statistique', '42G'),
+    new Statistique('ffdfs-789-htrey-yjrher', 'Tennis', '54F'),
+    new Statistique('gdggaeqq-77-grqr', 'Aikido', '31A')
+  ];
+
+constructor() {
+  setTimeout(() => {
+    let statAikido = new Statistique(
+    'gdggaeqq-77-grqr',
+    'Aikido',
+    '31A'
   )
-statTennis = new Statistique('ffdfs-789-htrey-yjrher', 'Tennis', '54F')
-title = 'evaluationStat'
+this.tabStat.push(statAikido);
+}, 2000)}
+  title = 'evaluationStat'
 }
